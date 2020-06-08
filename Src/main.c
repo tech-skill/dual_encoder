16795efdb02be9ec56f07708779337ab19c3d337
Frits Senders <sendie61@hotmail.com>
2020-06-08 11:34:11 +0200

    niet helemaal soepel...

--- Src/main.c
+++ Src/main.c
@@ -63,11 +63,11 @@ osThreadId task_100msHandle;
 osMessageQId advanceQueueHandle;
 /* USER CODE BEGIN PV */
 
-int32_t previousQuillHeight= 0;
-int32_t previousZHeight= 0;
+int32_t previousQuillHeight = 0;
+int32_t previousZHeight = 0;
 
-_Bool A[4] = {pdFALSE, pdTRUE, pdTRUE, pdFALSE};
-_Bool B[4] = {pdFALSE, pdFALSE, pdTRUE, pdTRUE};
+_Bool A[4] = { pdFALSE, pdTRUE, pdTRUE, pdFALSE };
+_Bool B[4] = { pdFALSE, pdFALSE, pdTRUE, pdTRUE };
 
 /* USER CODE END PV */
 
@@ -77,8 +77,8 @@ static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_USART1_UART_Init(void);
-void StartMainTask(void const * argument);
-void Starttask_100ms(void const * argument);
+void StartMainTask(void const *argument);
+void Starttask_100ms(void const *argument);
 
 /* USER CODE BEGIN PFP */
 
@@ -93,15 +93,13 @@ void Quadrature(int32_t advance);
 
 uint8_t QuillScale_data[NROFSCALEBYTES];
 
-
 /* USER CODE END 0 */
 
 /**
  * @brief  The application entry point.
  * @retval int
  */
-int main(void)
-{
+int main(void) {
 	/* USER CODE BEGIN 1 */
 
 	/* USER CODE END 1 */
@@ -173,8 +171,7 @@ int main(void)
 	/* We should never get here as control is now taken by the scheduler */
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
-	while (1)
-	{
+	while (1) {
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
@@ -186,10 +183,9 @@ int main(void)
  * @brief System Clock Configuration
  * @retval None
  */
-void SystemClock_Config(void)
-{
-	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
 	/** Initializes the CPU, AHB and APB busses clocks
 	 */
@@ -197,21 +193,19 @@ void SystemClock_Config(void)
 	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
 	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
 	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	{
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
 		Error_Handler();
 	}
 	/** Initializes the CPU, AHB and APB busses clocks
 	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
 	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
 	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
 	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-	{
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
 		Error_Handler();
 	}
 }
@@ -221,15 +215,14 @@ void SystemClock_Config(void)
  * @param None
  * @retval None
  */
-static void MX_TIM2_Init(void)
-{
+static void MX_TIM2_Init(void) {
 
 	/* USER CODE BEGIN TIM2_Init 0 */
 
 	/* USER CODE END TIM2_Init 0 */
 
-	TIM_Encoder_InitTypeDef sConfig = {0};
-	TIM_MasterConfigTypeDef sMasterConfig = {0};
+	TIM_Encoder_InitTypeDef sConfig = { 0 };
+	TIM_MasterConfigTypeDef sMasterConfig = { 0 };
 
 	/* USER CODE BEGIN TIM2_Init 1 */
 
@@ -249,14 +242,13 @@ static void MX_TIM2_Init(void)
 	sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
 	sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
 	sConfig.IC2Filter = 0;
-	if (HAL_TIM_Encoder_Init(&htim2, &sConfig) != HAL_OK)
-	{
+	if (HAL_TIM_Encoder_Init(&htim2, &sConfig) != HAL_OK) {
 		Error_Handler();
 	}
 	sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
 	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-	if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
-	{
+	if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig)
+			!= HAL_OK) {
 		Error_Handler();
 	}
 	/* USER CODE BEGIN TIM2_Init 2 */
@@ -270,8 +262,7 @@ static void MX_TIM2_Init(void)
  * @param None
  * @retval None
  */
-static void MX_USART1_UART_Init(void)
-{
+static void MX_USART1_UART_Init(void) {
 
 	/* USER CODE BEGIN USART1_Init 0 */
 
@@ -288,8 +279,7 @@ static void MX_USART1_UART_Init(void)
 	huart1.Init.Mode = UART_MODE_RX;
 	huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
 	huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-	if (HAL_UART_Init(&huart1) != HAL_OK)
-	{
+	if (HAL_UART_Init(&huart1) != HAL_OK) {
 		Error_Handler();
 	}
 	/* USER CODE BEGIN USART1_Init 2 */
@@ -301,8 +291,7 @@ static void MX_USART1_UART_Init(void)
 /** 
  * Enable DMA controller clock
  */
-static void MX_DMA_Init(void) 
-{
+static void MX_DMA_Init(void) {
 
 	/* DMA controller clock enable */
 	__HAL_RCC_DMA1_CLK_ENABLE();
@@ -319,9 +308,8 @@ static void MX_DMA_Init(void)
  * @param None
  * @retval None
  */
-static void MX_GPIO_Init(void)
-{
-	GPIO_InitTypeDef GPIO_InitStruct = {0};
+static void MX_GPIO_Init(void) {
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 
 	/* GPIO Ports Clock Enable */
 	__HAL_RCC_GPIOC_CLK_ENABLE();
@@ -331,7 +319,7 @@ static void MX_GPIO_Init(void)
 	HAL_GPIO_WritePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_RESET);
 
 	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOA, DRO_A_Pin|DRO_B_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, DRO_A_Pin | DRO_B_Pin, GPIO_PIN_RESET);
 
 	/*Configure GPIO pin : GREEN_LED_Pin */
 	GPIO_InitStruct.Pin = GREEN_LED_Pin;
@@ -341,7 +329,7 @@ static void MX_GPIO_Init(void)
 	HAL_GPIO_Init(GREEN_LED_GPIO_Port, &GPIO_InitStruct);
 
 	/*Configure GPIO pins : DRO_A_Pin DRO_B_Pin */
-	GPIO_InitStruct.Pin = DRO_A_Pin|DRO_B_Pin;
+	GPIO_InitStruct.Pin = DRO_A_Pin | DRO_B_Pin;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -352,74 +340,69 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 
 // QuillScale  message complete.
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart1)
-{
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart1) {
 	int32_t QuillHeight;
 
-	QuillHeight= quill2Micro();
-    int32_t Diff= previousQuillHeight-QuillHeight;
+	QuillHeight = quill2Micro();
+	int32_t Diff = previousQuillHeight - QuillHeight;
 
 	osMessagePut(advanceQueueHandle, Diff, 0);
-	previousQuillHeight= QuillHeight;
+	previousQuillHeight = QuillHeight;
 	HAL_UART_Receive_DMA(huart1, QuillScale_data, sizeof(QuillScale_data));
 
 }
 
-int32_t metric2Micro()
-{
-	int32_t Q= (QuillScale_data[1]-'0') * 1000000;
-	Q+=  (QuillScale_data[2]-'0') * 100000;
-	Q+=  (QuillScale_data[3]-'0') * 10000;
-	Q+=  (QuillScale_data[4]-'0') * 1000;
-	Q+=  (QuillScale_data[6]-'0') * 100;
-	Q+=  (QuillScale_data[7]-'0') * 10;
-	Q*=  (QuillScale_data[0]=='-')?-1:1;
+int32_t metric2Micro() {
+	int32_t Q = (QuillScale_data[1] - '0') * 1000000;
+	Q += (QuillScale_data[2] - '0') * 100000;
+	Q += (QuillScale_data[3] - '0') * 10000;
+	Q += (QuillScale_data[4] - '0') * 1000;
+	Q += (QuillScale_data[6] - '0') * 100;
+	Q += (QuillScale_data[7] - '0') * 10;
+	Q *= (QuillScale_data[0] == '-') ? -1 : 1;
 	return Q;
 
 }
 
-int32_t imperial2Micro()
-{
-	int32_t Q= (QuillScale_data[1]-'0') * 10000000;
-	Q+=  (QuillScale_data[2]-'0') * 1000000;
-	Q+=  (QuillScale_data[4]-'0') * 100000;
-	Q+=  (QuillScale_data[5]-'0') * 10000;
-	Q+=  (QuillScale_data[6]-'0') * 1000;
-	Q+=  (QuillScale_data[7]-'0') * 100;
-	Q*=  ((QuillScale_data[0]=='-')?-1:1)*254;
-	Q/=  10000;
+int32_t imperial2Micro() {
+	int32_t Q = (QuillScale_data[1] - '0') * 10000000;
+	Q += (QuillScale_data[2] - '0') * 1000000;
+	Q += (QuillScale_data[4] - '0') * 100000;
+	Q += (QuillScale_data[5] - '0') * 10000;
+	Q += (QuillScale_data[6] - '0') * 1000;
+	Q += (QuillScale_data[7] - '0') * 100;
+	Q *= ((QuillScale_data[0] == '-') ? -1 : 1) * 254;
+	Q /= 10000;
 	return Q;
 
 }
 
-int32_t quill2Micro()
-{
-	if( QuillScale_data[5]=='.')
+int32_t quill2Micro() {
+	if (QuillScale_data[5] == '.')
 		return metric2Micro();
-	if( QuillScale_data[3]=='.')
+	if (QuillScale_data[3] == '.')
 		return imperial2Micro();
 	else
 		return previousQuillHeight;
 
 }
 
-void Quadrature(int32_t count)
-{
-
-	  static uint8_t index = 0;
-	  int8_t dir = sign(count);
-	  // send AB signals
-	  for (int32_t counter = 0; counter < abs(count); counter++){
-	    index+=dir;
-	    index &= 0x3;
-	    HAL_GPIO_WritePin(GPIOA, DRO_A_Pin,A[index]?GPIO_PIN_SET:GPIO_PIN_RESET);
-	    HAL_GPIO_WritePin(GPIOA, DRO_B_Pin,B[index]?GPIO_PIN_SET:GPIO_PIN_RESET);
-	  }
+void Quadrature(int32_t count) {
+	static uint8_t index = 0;
+	int8_t dir = sign(count);
+	// send AB signals
+	for (int32_t counter = 0; counter < abs(count); counter++) {
+		index += dir;
+		index &= 0x3;
+		HAL_GPIO_WritePin(GPIOA, DRO_A_Pin,
+				A[index] ? GPIO_PIN_SET : GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA, DRO_B_Pin,
+				B[index] ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	}
 
 }
 
-void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
-{
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
 	__NOP();
 }
 
@@ -432,41 +415,37 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
  * @retval None
  */
 /* USER CODE END Header_StartMainTask */
-void StartMainTask(void const * argument)
-{
+void StartMainTask(void const *argument) {
 	/* USER CODE BEGIN 5 */
 	/* Infinite loop */
-	for(;;)
-	{
+	for (;;) {
 		osEvent Event = osMessageGet(advanceQueueHandle, 0);
-		int32_t Advance= (int32_t)(void*)Event.value.p;
+		int32_t Advance = (int32_t) (void*) Event.value.p;
 		Quadrature(Advance);
 	}
 	/* USER CODE END 5 */
 }
 
-/* USER CODE BEGIN Header_Starttask_100ms */
+/* USER CODE BEGIN Header_StartTask_100ms */
 /**
  * @brief Function implementing the task_100ms thread.
  * @param argument: Not used
  * @retval None
  */
-/* USER CODE END Header_Starttask_100ms */
-void Starttask_100ms(void const * argument)
-{
-	/* USER CODE BEGIN Starttask_100ms */
+/* USER CODE END Header_StartTask_100ms */
+void Starttask_100ms(void const *argument) {
+	/* USER CODE BEGIN StartTask_100ms */
 	HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_ALL);
 	/* Infinite loop */
-	for(;;)
-	{
-		int16_t ZHeight= TIM2->CNT*ENCODERRESOLUTION;
-		int32_t Diff= previousZHeight- ZHeight;
+	for (;;) {
+		int16_t ZHeight = TIM2->CNT * ENCODERRESOLUTION;
+		int32_t Diff = previousZHeight - ZHeight;
 
 		osMessagePut(advanceQueueHandle, Diff, 0);
-		previousZHeight= ZHeight;
+		previousZHeight = ZHeight;
 		osDelay(100);
 	}
-	/* USER CODE END Starttask_100ms */
+	/* USER CODE END StartTask_100ms */
 }
 
 /**
@@ -477,8 +456,7 @@ void Starttask_100ms(void const * argument)
  * @param  htim : TIM handle
  * @retval None
  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 	/* USER CODE BEGIN Callback 0 */
 
 	/* USER CODE END Callback 0 */
@@ -494,8 +472,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
  * @brief  This function is executed in case of error occurrence.
  * @retval None
  */
-void Error_Handler(void)
-{
+void Error_Handler(void) {
 	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 
